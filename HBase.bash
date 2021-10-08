33a9dc191e61f5e9d06b8d3effebf871407857d9
Alladio Bonesso <Alladio@users.noreply.github.com>
2021-10-07 22:18:33 -0300

    introdução comando Hbase

--- HBase.bash
+++ HBase.bash
@@ -2,3 +2,23 @@
 
 #Visão geral do Hbase no Shell
 beeline -u jdbc:hive2://localhost:10000
+#poderia usar o HIVE, também
+
+
+hbase shell
+
+#quando for parar os serviços parar eles de forma correta
+./stop_all
+
+#se entrar no SafeMode | Como se fosse um Fdisk
+sudo -u hdfs hadoop fsck / | egrep -v '^/.+$' | grep -v eplica
+
+
+#Caso dê erros
+sudo -u hdfs hdfs dfs -rm  “caminho do arquivo(tirar aspas)”
+
+
+#Ver as operações que acontecem no cluster do Hadoop
+yarn top 
+
+
